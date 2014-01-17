82b743e5827b32f12535df9167e5c06be12167a7
pweesp <peter.weesp@gmail.com>
2014-01-17 22:24:33 +0100

    Initial version

---
+++ src/nl/test/Person.java
@@ -0,0 +1,25 @@
+package nl.test;
+
+public class Person {
+
+	public String getFirstname() {
+		return firstname;
+	}
+	
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	
+	public String getLastname() {
+		return lastname;
+	}
+	
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	
+	private String firstname;
+	private String lastname;
+	
+	
+}
